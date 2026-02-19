f17cd40ca0f43e046e1b2da7f2c57d97ffd5850a
Alexander <pchp250g8@tutamail.com>
2026-02-19 14:07:31 +0300

    Добавьте файлы проекта.

---
+++ DecToBin/Module1.vb
@@ -0,0 +1,30 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim uNumber As UInteger
+        Console.WriteLine("Input an unsigned integer number")
+        Dim bIsRightNumber = (UInteger.TryParse(Console.ReadLine(), uNumber)) AndAlso
+                     (uNumber <= UInteger.MaxValue)
+        If (Not bIsRightNumber) Then
+            Console.WriteLine("Invalid number format or number too big")
+            Console.Read()
+            Return
+        End If
+        Dim strBinNum = ""
+        Dim uTempVal = uNumber
+        Do While (uTempVal > 0)
+            strBinNum = CInt(uTempVal Mod 2).ToString() + strBinNum
+            uTempVal /= 2
+        Loop
+        If (strBinNum.Length = 0) Then
+            strBinNum = "0"
+        End If
+        Console.WriteLine _
+        (
+            "The binary equavalent of the decimal number {0} is: {1}",
+            uNumber, strBinNum
+        )
+        Console.Read()
+    End Sub
+
+End Module
---
+++ DecToBin/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором
+' набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+' связанные со сборкой.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("DecToBin")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("DecToBin")>
+<Assembly: AssemblyCopyright("Copyright ©  2026")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("3c394e0d-a9e9-4458-a95f-a123104d8718")>
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии
+'   Номер сборки
+'      Редакция
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
